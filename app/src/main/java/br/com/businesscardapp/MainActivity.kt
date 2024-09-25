bec31ef727efb074e561e31a97270baa79897492
Breno Ferreira <brenohenrique.oliveiraferreira@gmail.com>
2024-09-25 14:02:23 -0300

    Jetpack Compose Project

---
+++ app/src/main/java/br/com/businesscardapp/MainActivity.kt
@@ -0,0 +1,153 @@
+package br.com.businesscardapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.businesscardapp.ui.theme.BusinessCardAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BusinessCardAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BusinessCard() {
+    Column {
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            MainInfoSection(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                name = stringResource(R.string.dev_name),
+                role = stringResource(R.string.dev_role),
+            )
+        }
+        Row(
+            modifier = Modifier.weight(0.3f)
+        ) {
+            ContactInfoSection()
+        }
+    }
+}
+
+@Composable
+fun MainInfoSection(
+    painter: Painter,
+    name: String,
+    role: String,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth().padding(start = 12.dp, end = 12.dp, bottom = 12.dp)
+        )
+        Text(
+            text = name,
+            fontSize = 36.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(start = 12.dp, end = 12.dp, bottom = 12.dp)
+        )
+        Text(
+            text = role,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp)
+        )
+    }
+}
+
+@Composable
+fun ContactInfoSection() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize().padding(24.dp)
+    ) {
+        ContactInfoRow(
+            icon = Icons.Rounded.Phone,
+            text = stringResource(R.string.dev_phone_number),
+            modifier = Modifier.weight(1f)
+        )
+        ContactInfoRow(
+            icon = Icons.Rounded.Share,
+            text = stringResource(R.string.dev_share_link),
+            modifier = Modifier.weight(1f),
+        )
+        ContactInfoRow(
+            icon = Icons.Rounded.Email,
+            text = stringResource(R.string.dev_email),
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun ContactInfoRow(icon: ImageVector, text: String, modifier: Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxWidth().padding(vertical = 3.dp, horizontal = 6.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            Modifier.padding(end = 12.dp).size(32.dp)
+        )
+        Text(
+            text = text,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BusinessCardPreview() {
+    BusinessCardAppTheme {
+        BusinessCard()
+    }
+}
